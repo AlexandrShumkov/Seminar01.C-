1c65428f3c70b43e06ee36216a1410ba4e10e383
Alexandr <alshumkov2@gmail.com>
2021-10-31 17:17:48 +0300

    Changes

--- Prog03/Program.cs
+++ Prog03/Program.cs
@@ -1,11 +1,10 @@
-﻿int s=0, i=1;
-while (i <= 15)
+﻿int s=0,i=1;
+while(i<=15)
 {
-    if (i % 2 == 0);
+    if(i%2==0)
     {
-        s = s + i;
+        s=s+i;
     }
     ++i;
 }
 Console.WriteLine(s);
-
